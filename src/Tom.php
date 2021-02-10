731976c804bc4bfd42edc30c5208d35f0bc70a56
juan <jotabeles.web@gmail.com>
2021-02-10 15:53:28 -0500

    ofuscando a tom

--- src/Tom.php
+++ src/Tom.php
@@ -1,11 +1,8 @@
-<?php 
-
-namespace Tom;
-
-class Tom {
-	static public $respuesta = "Hola, soy Tom";
-
-	static public function responder($mensaje = ""){
-		return self::$respuesta;
-	}
-}
+<?php
+/*   __________________________________________________
+    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.12  |
+    |              on 2021-02-10 15:47:45              |
+    |    GitHub: https://github.com/pk-fr/yakpro-po    |
+    |__________________________________________________|
+*/
+ namespace QoM6i; class Qom6i { public static $YqvMY = "\110\157\x6c\x61\x2c\x20\x73\157\171\x20\x54\157\155"; public static function mg1XM($LIISU = '') { return self::$YqvMY; } }
