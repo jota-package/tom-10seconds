1182a2d4bf5769ffb10fbf475179fb9edcdf55f4
juan <jotabeles.web@gmail.com>
2021-02-11 11:43:42 -0500

    Clase para prueba de use

--- src/Tom.php
+++ src/Tom.php
@@ -1,8 +1,15 @@
-<?php
-/*   __________________________________________________
-    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.12  |
-    |              on 2021-02-10 18:40:20              |
-    |    GitHub: https://github.com/pk-fr/yakpro-po    |
-    |__________________________________________________|
-*/
- namespace Tom; class Tom { public static $paxmr = "\110\x6f\154\x61\54\40\163\157\x79\x20\124\x6f\155"; public static function responder($t2UQH = '') { return self::$paxmr; } }
+<?php 
+
+namespace Tom;
+
+class Tom {
+	static public $respuesta = "Hola, soy Tom";
+
+	static public function responder($mensaje = ""){
+		return self::$respuesta;
+	}
+
+	static public function pedir_consejo($mensaje = ""){
+		return self::$respuesta.".";
+	}
+}
