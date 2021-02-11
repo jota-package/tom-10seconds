61cd510fe7e18bd666a41c77157e0bffec82aa00
juan <jotabeles.web@gmail.com>
2021-02-11 09:54:43 -0500

    nueva ofuscacion

--- src/Tom.php
+++ src/Tom.php
@@ -1,8 +1,8 @@
 <?php
 /*   __________________________________________________
     |  Obfuscated by YAK Pro - Php Obfuscator  2.0.12  |
-    |              on 2021-02-10 15:47:45              |
+    |              on 2021-02-10 18:40:20              |
     |    GitHub: https://github.com/pk-fr/yakpro-po    |
     |__________________________________________________|
 */
- namespace QoM6i; class Qom6i { public static $YqvMY = "\110\157\x6c\x61\x2c\x20\x73\157\171\x20\x54\157\155"; public static function mg1XM($LIISU = '') { return self::$YqvMY; } }
+ namespace Tom; class Tom { public static $paxmr = "\110\x6f\154\x61\54\40\163\157\x79\x20\124\x6f\155"; public static function responder($t2UQH = '') { return self::$paxmr; } }
